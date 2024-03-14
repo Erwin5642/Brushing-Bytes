ca6c009885a6a81981d8e13a67c9be50a58e1261
Johannes <135025081+Erwin5642@users.noreply.github.com>
2024-03-14 13:14:34 -0400

    Update alglilib.cpp

--- Principal/alglilib.cpp
+++ Principal/alglilib.cpp
@@ -1,1 +1,27 @@
 //Biblioteca de Álgebra Linear
+
+#include <cmath>
+using namespace std;
+
+class fracao
+{
+private:
+    int den;
+    int num;
+public:
+
+    void simplify(){
+        int i;
+
+        for (i = 2; i < sqrt(den > num ? den : num); i++)
+        {
+            if((den % i == 0) && (num % i ==0)){
+                den /= i;
+                num /= i;
+            }
+        }
+        
+    }
+
+};
+
