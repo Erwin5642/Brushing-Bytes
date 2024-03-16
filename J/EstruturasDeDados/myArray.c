e78f509f2836f5685b850201cf634701a80e8def
ErinHattori <joaovitor14mju@gmail.com>
2024-03-16 12:19:46 -0400

    Atualizações no vetor

--- J/EstruturasDeDados/myArray.c
+++ J/EstruturasDeDados/myArray.c
@@ -1,6 +1,26 @@
+#include <stdlib.h>
 //Vetor
 //Operações: read - O(1), insert - O(n), delete - (n)
-
 typedef struct V{
-    
+    int *v;
+
+    int n;
 }Vetor;
+
+
+void inicializarVetor(Vetor vetor){
+    vetor.v = malloc(sizeof(int));
+    vetor.v[0] = 0;
+    vetor.n = 0;
+}
+
+void inserirVetor(Vetor vetor, int x){
+    int i;
+    vetor.n++;
+    vetor.v = realloc(vetor.v, sizeof(int) * vetor.n);
+
+}
+
+void encerrarVetor(Vetor vetor){
+    free(vetor.v);
+}
