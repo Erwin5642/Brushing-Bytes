4ce509e1da6478032c33737e5b6dc127bc2439fa
ErinHattori <joaovitor14mju@gmail.com>
2024-03-16 21:46:37 -0400

    Funcoes imcompletas de Lista Encadeada implementadas

--- J/EstruturasDeDados/myList.c.c
+++ J/EstruturasDeDados/myList.c.c
@@ -1,8 +1,32 @@
+#include <stdlib.h>
 //ista
 //Operações: read - O(n), insert - O(1), delete - O(1)
 typedef struct L{
-
+    int chave;
+    ListaEncadeada *prox;
 }ListaEncadeada;
+
+void inicializarLista(ListaEncadeada lista){
+    lista.chave = 0;
+    lista.prox = NULL;
+}
+
+void encerrarLista(ListaEncadeada lista){
+    if(lista.prox != NULL){
+        encerrarLista(*lista.prox);
+        free(lista.prox);
+    }
+}
+
+void inserirLista(ListaEncadeada lista, int x){
+    ListaEncadeada *new = malloc(sizeof(ListaEncadeada));
+    new->chave = x;
+    if(x <= lista.chave){
+        new->prox = lista.prox;
+        lista.prox = new; 
+    }
+}
+
 
 typedef struct LD{
 
