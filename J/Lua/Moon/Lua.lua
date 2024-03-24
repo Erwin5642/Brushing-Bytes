0b71e5e378ccca0981e8729e50c0a1b013d57d05
ErinHattori <joaovitor14mju@gmail.com>
2024-03-24 14:59:11 -0400

    Jogador implementado

---
+++ J/Lua/Moon/Lua.lua
@@ -0,0 +1,31 @@
+Lua = Class{}
+
+local VELOCIDADE_GRAVIDADE = 30
+
+--Objetivo: construtor da classe Lua
+function Lua:init()
+    --Linka a imagem do jogador com a sua classe
+    self.imagem = love.graphics.newImage('Imagens/Lua-1.png')
+    --Coloca o jogador no meio da janela 
+    self.x = gLARGURA_JANELA/2 - 8
+    self.y = gALTURA_JANELA/2 - 8  
+    self.dy = 0
+end
+
+--Objetivo: implementa a movimentação do jogador
+function Lua:update(dt)
+    --Altera a velocidade do jogador na medida em que o tempo passa e atualiza a sua posição
+    self.dy = self.dy + VELOCIDADE_GRAVIDADE * dt
+
+    if love.keyboard.wasPressed('space') then
+        self.dy = -10
+    end
+
+    self.y = self.y + self.dy    
+end
+
+--Objetivo: passa os parametros para desenhar o jogador na tela
+function Lua:render()
+    --Desenha o jogador na tela
+    love.graphics.draw(self.imagem, self.x, self.y)
+end
--- J/Lua/Moon/main.lua
+++ J/Lua/Moon/main.lua
@@ -1,3 +1,4 @@
+--Chama todos as classes usadas no programa
 Class = require 'class'
 require 'Lua'
 require 'Obstaculo'
@@ -13,33 +14,58 @@ require 'Estados/Countdown'
 -- Define o tamanho da janela do jogo
 gALTURA_JANELA = 600
 gLARGURA_JANELA = 800
-
+--Define as variaveis usadas para representar o background
 local background = love.graphics.newImage('Imagens/Background-2.jpg')
 local VELOCIDADE_BACKGROUND = 60
 local loop_scroll = 0
 
+--Objetivo: incialia certas variaveis para o funcionamento do código
 function love.load()
     love.window.setMode(gLARGURA_JANELA, gALTURA_JANELA, {
         resizable = false,
         fullscreen = false
     })
     love.window.setTitle('Moon Light')
+    --Cria uma table vazia para armazenar teclas pressionadas pelo usuário
+    love.keyboard.keysPressed = {}
+    lua = Lua()
 end
 
-function love.keypreessed(key)
+--Objetivo: realizar alguma ação declarada no escopo da função sempre que uma tecla for acionada
+function love.keypressed(key)
+    --Sempre que uma dada tecla é apertada, adciona um índice para ela na table
+    love.keyboard.keysPressed[key] = true
+    --Caso o usuário aperte esc encerra o app
     if key == 'escape' then
         love.event.quit()
     end
 end
 
+--Objetivo: verifica se uma dada tecla foi verificada
+function love.keyboard.wasPressed(key)
+    --Retorna verdadeiro se a tecla no indice key foi apertada
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+--Objetivo: atualiza certas variaveis do programa a cada frame 
 function love.update(dt)
     -- Atualiza a posição do background
     loop_scroll = (loop_scroll + VELOCIDADE_BACKGROUND * dt) % background:getWidth()
+    lua:update(dt)
+
+    --Apaga as teclas que acabaram de serem apertadas
+    love.keyboard.keysPressed = {}
 end
 
+--Objetivo: desenha imagens na tela
 function love.draw()
     -- Desenha o background uma vez e então desenha uma segunda vez onde a primeira imagem acaba
     love.graphics.draw(background, -loop_scroll, 0, 0, 1, gALTURA_JANELA / background:getHeight())
     love.graphics.draw(background, -loop_scroll + background:getWidth(), 0, 0, 1,
         gALTURA_JANELA / background:getHeight())
+    lua:render()
 end
