62b9385affe61a2fc513f9fcf852a383a46f2d0f
ErinHattori <joaovitor14mju@gmail.com>
2024-03-29 11:12:24 -0400

    Teste de renderização com função linear

--- J/Lua/CartesianChronicles/Personagem.lua
+++ J/Lua/CartesianChronicles/Personagem.lua
@@ -4,6 +4,7 @@ function Personagem:init(x, y, r)
     self.x = x
     self.y = y
     self.r = r
+    self.m = -1
 end
 
 function Personagem:update(dt)
@@ -13,4 +14,9 @@ end
 function Personagem:render()
     love.graphics.setColor(1, 1, 1)
     love.graphics.circle('fill', self.x, self.y, self.r)
+    love.graphics.line(-((math.sqrt(4 * self.m * self.m * gRAIO_FUNCAO * gRAIO_FUNCAO + 1) - 2 * self.m * self.m * self.x + 1)/(2 * self.m * self.m)), 
+        -((math.sqrt(4 * self.m * self.m * gRAIO_FUNCAO * gRAIO_FUNCAO + 1) - 2 * self.m * self.y + 1)/(2 * self.m)), 
+        (math.sqrt(4 * self.m * self.m * gRAIO_FUNCAO * gRAIO_FUNCAO + 1) + 2 * self.m * self.m * self.x - 1)/(2 * self.m * self.m),
+        (math.sqrt(4 * self.m * self.m * gRAIO_FUNCAO * gRAIO_FUNCAO + 1) + 2 * self.m * self.y - 1)/(2 * self.m)
+    ) 
 end
--- J/Lua/CartesianChronicles/main.lua
+++ J/Lua/CartesianChronicles/main.lua
@@ -6,16 +6,17 @@ require 'Inimigo'
 gALTURA_TELA = 600
 gLARGURA_TELA = 800
 gRAIO_PERSONAGEM = 40
-gRAIO_FUNCAO = 3
+gRAIO_FUNCAO = 160
 
 function love.load()
     love.window.setTitle('Cartesian Chronicles')
     love.window.setMode(gLARGURA_TELA, gALTURA_TELA, {fullscreen = false, resizable = false})
-    fx = Personagem(gLARGURA_TELA / 2, gALTURA_TELA / 2, 100)
+    fx = Personagem(gLARGURA_TELA / 2, gALTURA_TELA / 2, gRAIO_PERSONAGEM)
 end
 
+
 function love.keypressed(key)
-    if key == 'scape' then
+    if key == 'escape' then
         love.event.quit()
     end
 end
