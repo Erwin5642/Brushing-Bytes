dfb0cf4fdca9ed4d392961f09bdd4f43b6c64287
ErinHattori <joaovitor14mju@gmail.com>
2024-04-19 19:43:10 -0300

    Reset na lista encadeada

--- J/C/EstruturasDeDados/myList.c
+++ J/C/EstruturasDeDados/myList.c
@@ -1,91 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct ListaEncadeada
+struct s_no
 {
-    int chave;
-    struct ListaEncadeada *prox;
-} ListaEncadeada;
+	int chave;
+	struct s_no *prox;
+};
 
-void insere(ListaEncadeada **inicio, int val)
+void insere(struct s_no **inicio, int val)
 {
-    ListaEncadeada *p = malloc(sizeof(ListaEncadeada));
-    p->chave = val;
-    p->prox = *inicio;
-    *inicio = p;
-}
-
-ListaEncadeada *buscaVal(ListaEncadeada *inicio, int val)
-{
-    if (inicio != NULL)
-    {
-        printf("\nNumero atual %d\n", inicio->chave);
-        if (val == inicio->chave)
-        {
-            return inicio;
-        }
-        return buscaVal(inicio->prox, val);
-    }
-    return NULL;
-}
-
-void remover(ListaEncadeada **inicio, int val)
-{
-    ListaEncadeada *p, *aux;
-    p = buscaVal(*inicio, val);
-    if (p != NULL)
-    {
-        aux = p->prox;
-        printf("\nRemovido o numero %d\n", p->chave);
-        free(p);
-        p = aux;
-    }
-}
-
-void desalocarLista(ListaEncadeada *inicio)
-{
-    if (inicio != NULL)
-    {
-        if (inicio->prox != NULL)
-        {
-            desalocarLista(inicio->prox);
-        }
-        printf("\nLiberado o numero %d\n", inicio->chave);
-        free(inicio);
-    }
+	struct s_no *p = malloc(sizeof(struct s_no));
+	p->chave = val;
+	p->prox = *inicio;
+	*inicio = p;
 }
 
 int main()
 {
-    ListaEncadeada *L = NULL;
-    ListaEncadeada *p;
-
-    insere(&L, 4);
-    insere(&L, 3);
-    insere(&L, 2);
-    insere(&L, 1);
-    insere(&L, 0);
-
-    p = L;
-    while (p != NULL)
-    {
-        printf("[%i]->", p->chave);
-        p = p->prox;
-    }
-
-    printf("\n");
-
-    remover(&L, 2);
-    printf("\nSaiu\n");
-    p = L;
-    while (p != NULL)
-    {
-        printf("\nEntrou\n");
-        printf("[%i]->", p->chave);
-        p = p->prox;
-    }
-
-    desalocarLista(L);
-
-    return 0;
+	struct s_no *L = NULL;
+	struct s_no *p;
+	
+	insere(&L, 4);
+	insere(&L, 3);
+	insere(&L, 2);
+	insere(&L, 1);
+	insere(&L, 0);
+		
+	p = L;
+	while( p != NULL )
+	{
+		printf("[%i]->", p->chave);
+		p = p->prox;
+	}	
+	
+	printf("\n");
+	
+	return 0;
 }
