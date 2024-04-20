fae881fcac9ca958ff70b99173d59687e63d5c16
ErinHattori <joaovitor14mju@gmail.com>
2024-04-19 22:04:33 -0300

    Remoção lista funcional

--- J/C/EstruturasDeDados/myList.c
+++ J/C/EstruturasDeDados/myList.c
@@ -15,25 +15,72 @@ void insere(struct s_no **inicio, int val)
 	*inicio = p;
 }
 
+void deleta(struct s_no **inicio, int val)
+{
+	struct s_no *aux;
+	if ((*inicio) != NULL)
+	{
+		if ((*inicio)->chave != val)
+		{
+			deleta(&(*inicio)->prox, val);
+		}
+		else
+		{
+			aux = (*inicio)->prox;
+			free(*inicio);
+			(*inicio) = aux;
+		}
+	}
+}
+
+void imprime(struct s_no *inicio){
+	struct s_no *p = inicio;
+	while (p != NULL)
+	{
+		printf("[%i]->", p->chave);
+		p = p->prox;
+	}
+	printf("\n");
+}
+
 int main()
 {
 	struct s_no *L = NULL;
-	struct s_no *p;
-	
 	insere(&L, 4);
 	insere(&L, 3);
 	insere(&L, 2);
 	insere(&L, 1);
 	insere(&L, 0);
-		
-	p = L;
-	while( p != NULL )
-	{
-		printf("[%i]->", p->chave);
-		p = p->prox;
-	}	
-	
-	printf("\n");
-	
+
+	imprime(L);
+
+	deleta(&L, 2);
+
+	imprime(L);
+
+	deleta(&L, 2);
+
+	imprime(L);
+
+	deleta(&L, 1);
+
+	imprime(L);
+
+	deleta(&L, 0);
+
+	imprime(L);
+
+	deleta(&L, 4);
+
+	imprime(L);
+
+	deleta(&L, 3);
+
+	imprime(L);
+
+	deleta(&L, 3);
+
+	imprime(L);
+
 	return 0;
 }
