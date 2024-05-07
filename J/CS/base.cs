e83309e1be497de595d8f589f92e0da81ffbf907
Erin <joaovitor14mju@gmail.com>
2024-05-06 23:56:00 -0300

    Cs begin

---
+++ J/CS/base.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello world!\n");
+            Console.ReadKey();
+        }
+    }
+}
