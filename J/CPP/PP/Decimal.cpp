97a7951dcc2b504eb70b8749164a27cf37bba874
Alice <joaovitor14mju@gmail.com>
2024-06-21 20:54:06 -0400

    Mais lista de PP

--- J/CPP/PP/Decimal.cpp
+++ J/CPP/PP/Decimal.cpp
@@ -57,28 +57,88 @@ public:
         memcpy(str, src.str, size + 1);
         return *this;
     }
+    String &operator=(const char src[])
+    {
+        if (size != strlen(src))
+        {
+            resize(strlen(src));
+        }
+        memcpy(str, src, size + 1);
+        return *this;
+    }
+    String &operator=(const char src)
+    {
+        char src2[2];
+        src2[0] = src;
+        src2[1] = '\0';
+        if (size != 1)
+        {
+            resize(1);
+        }
+        memcpy(str, src2, 2);
+        return *this;
+    }
     String operator+(const String src)
     {
         resize(src.size + size);
         strncat(str, src.str, src.size + 1);
         return *this;
     }
+    String operator+(const char src[])
+    {
+        resize(strlen(src) + size);
+        strncat(str, src, strlen(src) + 1);
+        return *this;
+    }
+    String operator+(const char src)
+    {
+        char src2[2];
+        src2[0] = src;
+        src2[1] = '\0';
+        resize(size + 1);
+        strncat(str, src2, 2);
+        return *this;
+    }
     char &operator[](const unsigned int i)
     {
-        if(i < size){
+        if (i < size)
+        {
             return str[i];
         }
-        else{
+        else
+        {
             return str[size];
         }
     }
-    unsigned int getSize() const{
+    unsigned int getSize() const
+    {
         return size;
     }
-    bool operator==(const String cmp) const {
+    bool operator==(const String cmp) const
+    {
         return strncmp(str, cmp.str, size) == 0;
     }
-    bool isEmpty() const {
+    bool isEmpty() const
+    {
         return size == 0;
+    }
+};
+
+class Decimal
+{
+private:
+    int num;
+
+public:
+    Decimal(int num = 0) : num(num) {}
+    String hexadecimal() const {
+        String ret, concat;
+        char a;
+        int n = num;
+        while(n){
+            concat = '0' + num % 16;
+            ret = concat + ret; 
+            concat = '\0';
+        }
     }
 };
