66cb0c27b86761d29052c506ce89d3cddd90ea4e
Alice <joaovitor14mju@gmail.com>
2024-07-22 21:02:32 -0400

    T3 adequado ao livro

--- J/C/Trabalhos/AED-II/T3.c
+++ J/C/Trabalhos/AED-II/T3.c
@@ -1,9 +1,7 @@
 // Nome: João Vitor Antunes da Silva         RGM: 48935
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "gfx.h"
-#include <time.h>
 
 #define g_NODE_WIDTH 30
 #define g_NODE_HEIGHT 30
@@ -13,15 +11,15 @@
 #define g_Y_TREE_ORIGIN 30
 #define g_NODES_DISTANCE 75
 
-typedef struct Tree
+typedef struct BinaryTree
 {
     int key;
-    struct Tree *left;
-    struct Tree *right;
-} Tree;
+    struct BinaryTree *left;
+    struct BinaryTree *right;
+} BinaryTree;
 
 // Desaloca todos os nós de uma árvore binária de busca
-void deleteSearchTree(Tree **T)
+void deleteBinaryTree(BinaryTree **T)
 {
     if (T)
     {
@@ -29,11 +27,11 @@ void deleteSearchTree(Tree **T)
         {
             if ((*T)->left)
             {
-                deleteSearchTree(&(*T)->left);
+                deleteBinaryTree(&(*T)->left);
             }
             if ((*T)->right)
             {
-                deleteSearchTree(&(*T)->right);
+                deleteBinaryTree(&(*T)->right);
             }
             free((*T));
             *T = NULL;
@@ -41,81 +39,133 @@ void deleteSearchTree(Tree **T)
     }
 }
 
-Tree *newNodeSearchTree(int key)
+// Retorna um ponteiro para o nó com valor buscado ou NULL caso ele não exista na árvore binária de busca
+BinaryTree *searchkeyBinaryTree(BinaryTree *root, int key, int *flag)
 {
-    Tree *aux = malloc(sizeof(Tree));
+    if (!root)
+    {
+        *flag = 0;
+    }
+    while (root)
+    {
+        if (key == root->key)
+        {
+            *flag = 1;
+            return root;
+        }
+        else if (key < root->key)
+        {
+            if (!root->left)
+            {
+                *flag = 2;
+                return root;
+            }
+            root = root->left;
+        }
+        else
+        {
+            if (!root->right)
+            {
+                *flag = 3;
+                return root;
+            }
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+BinaryTree *newNodeBinaryTree(int key)
+{
+    BinaryTree *aux = malloc(sizeof(BinaryTree));
     aux->key = key;
     aux->left = aux->right = NULL;
     return aux;
 }
 
 // Insere um valor numa árvore binária de busca
-void insertkeySearchTree(Tree **T, int key)
+int insertKeyBinaryTree(BinaryTree **root, int key)
 {
-    if (T)
+    int flag;
+    BinaryTree *new;
+    if (root)
     {
-        while (*T)
+        new = searchkeyBinaryTree(*root, key, &flag);
+        if (flag != 1)
         {
-            if ((*T)->key > key)
+            if (flag == 0)
             {
-                T = &(*T)->left;
+                *root = newNodeBinaryTree(key);
+            }
+            else if (flag == 2)
+            {
+                new->left = newNodeBinaryTree(key);
             }
             else
             {
-                T = &(*T)->right;
+                new->right = newNodeBinaryTree(key);
             }
+            return 1;
         }
-        (*T) = newNodeSearchTree(key);
     }
+    return 0;
 }
 
-// Remove um valor de uma árvore binária de busca
-int removekeySearchTree(Tree **T, int key)
+BinaryTree *removeNodeBinaryTree(BinaryTree *node)
 {
-    Tree *aux;
-    if (T)
+    BinaryTree *aux, **sucessorPointer;
+    if (!node->left)
     {
-        while (*T)
+        aux = node->right;
+        free(node);
+        return aux;
+    }
+    if (!node->right)
+    {
+        aux = node->left;
+        free(node);
+        return aux;
+    }
+    // Encontra o sucessor do nó a ser removido
+    sucessorPointer = &node->right;
+    while ((*sucessorPointer)->left)
+    {
+        sucessorPointer = &(*sucessorPointer)->left;
+    }
+    // Faz as trocas necessárias com os ponteiros e desaloca o nó
+    (*sucessorPointer)->left = node->left;
+    aux = *sucessorPointer;
+    *sucessorPointer = (*sucessorPointer)->right;
+    aux->right = node->right;
+    free(node);
+
+    return aux;
+}
+
+// Remove uma chave de uma árvore binária de busca
+// Retorna 1 num sucesso e 0 se houver algum erro
+int removeKeyBinaryTree(BinaryTree **root, int key)
+{
+    if (root)
+    {
+        // Procura pelo nó com a chave desejada
+        while (*root)
         {
-            if ((*T)->key == key)
+            if (key < (*root)->key)
             {
-                aux = *T;
-                if ((*T)->left && (*T)->right)
-                {
-                    T = &(*T)->right;
-                    while ((*T)->left)
-                    {
-                        T = &(*T)->left;
-                    }
-                    aux->key = (*T)->key;
-                    key = aux->key;
-                }
-                else if ((*T)->left)
-                {
-                    *T = (*T)->left;
-                    free(aux);
-                    return 1;
-                }
-                else if ((*T)->right)
-                {
-                    *T = (*T)->right;
-                    free(aux);
-                    return 1;
-                }
-                else
-                {
-                    free(*T);
-                    *T = NULL;
-                    return 1;
-                }
+                // Se for menor busca na sub arvore esquerda
+                root = &(*root)->left;
             }
-            else if ((*T)->key > key)
+            else if (key > (*root)->key)
             {
-                T = &(*T)->left;
+                // Se for maior busca na sub arvore direita
+                root = &(*root)->right;
             }
-            else if ((*T)->key < key)
+            else
             {
-                T = &(*T)->right;
+                // Se encontrar, remove o nó de forma adequada
+                *root = removeNodeBinaryTree(*root);
+                return 1;
             }
         }
     }
@@ -123,7 +173,7 @@ int removekeySearchTree(Tree **T, int key)
 }
 
 // Retorna um ponteiro para o menor nó na árvore binária de busca
-Tree *minSearchTree(Tree *T)
+BinaryTree *minBinaryTree(BinaryTree *T)
 {
     if (T)
     {
@@ -132,15 +182,12 @@ Tree *minSearchTree(Tree *T)
         {
             T = T->left;
         }
-        // Retorna o nó com o menor valor
-        return T;
     }
-    // Retorna NULL se a árvore era vazia
-    return NULL;
+    return T;
 }
 
 // Retorna um ponteiro para o maior nó na árvore binária de busca
-Tree *maxSearchTree(Tree *T)
+BinaryTree *maxBinaryTree(BinaryTree *T)
 {
     if (T)
     {
@@ -149,41 +196,14 @@ Tree *maxSearchTree(Tree *T)
         {
             T = T->right;
         }
-        // Retorna o nó com o maior valor
-        return T;
     }
-    // Retorna NULL se a árvore era vazia
-    return NULL;
-}
-
-// Retorna um ponteiro para o nó com valor buscado ou NULL caso ele não exista na árvore binária de busca
-Tree *searchkeySearchTree(Tree *T, int key)
-{
-    // Percorre a árvore enquanto ela não vazia ou houverem valores possíveis para se analisar
-    while (T)
-    {
-        // Se o valor buscado estiver no nó ou nos nós a esquerda ou direita, retorna o endereço deles
-        if (T->key == key)
-        {
-            return T;
-        }
-        if (T->key > key)
-        {
-            T = T->left;
-        }
-        else
-        {
-            T = T->right;
-        }
-    }
-    // Se a árvore era vazia ou o valor não encontrado retorna NULL
-    return NULL;
+    return T;
 }
 
 // Retorna um ponteiro para o sucessor de um dado valor na árvore binária de busca
-Tree *sucessorNodeSearchTree(Tree *T, int key)
+BinaryTree *sucessorNodeBinaryTree(BinaryTree *T, int key)
 {
-    Tree *ancestralNode = NULL;
+    BinaryTree *ancestralNode = NULL;
     // Se a árvore não for vazia efetua uma busca pelo valor que se deseja descobrir o sucessor
     // Enquanto mantém um ponteiro para o último nó em que foi efetuado uma "virada para esquerda"
     // Que será o ancestral mais baixo que possui um nó filho à esquerda que é ancestral do nó buscado
@@ -198,13 +218,13 @@ Tree *sucessorNodeSearchTree(Tree *T, int key)
             ancestralNode = T;
             T = T->left;
         }
-        else if (T->right)
+        else if (T->right && T->key == key)
         {
             // No caso do nó com o valor buscado tiver filho à direita, o sucessor
             // será o menor valor na sub-árvore direita dele
-            return minSearchTree(T->right);
+            return minBinaryTree(T->right);
         }
-        else
+        else if (T->key == key)
         {
             // Caso não exista a sub-árvore direita, o sucessor será o nó ancestral que foi mantido durante a busca
             return ancestralNode;
@@ -214,9 +234,9 @@ Tree *sucessorNodeSearchTree(Tree *T, int key)
 }
 
 // Retorna um ponteiro para o antecessor de um dado valor na árvore binária de busca
-Tree *predecessorNodeSearchTree(Tree *T, int key)
+BinaryTree *predecessorNodeBinaryTree(BinaryTree *T, int key)
 {
-    Tree *ancestralNode = NULL;
+    BinaryTree *ancestralNode = NULL;
     // Se a árvore não for vazia efetua uma busca pelo valor que se deseja descobrir o antecessor
     // Enquanto mantém um ponteiro para o último nó em que foi efetuado uma "virada para direita"
     // Que será o ancestral mais baixo que possui um nó filho à direita que é ancestral do nó buscado
@@ -231,13 +251,13 @@ Tree *predecessorNodeSearchTree(Tree *T, int key)
         {
             T = T->left;
         }
-        else if (T->left)
+        else if (T->left && T->key == key)
         {
             // No caso do nó com o valor buscado tiver filho à direita, o antecessor
             // será o maior valor na sub-árvore esquerda dele
-            return maxSearchTree(T->left);
+            return maxBinaryTree(T->left);
         }
-        else
+        else if (T->key == key)
         {
             // Caso não exista a sub-árvore esquerda, o antecessor será o nó ancestral que foi mantido durante a busca
             return ancestralNode;
@@ -278,7 +298,7 @@ FILE *openFile(const char *name, const char *mode)
     return file;
 }
 
-void saveNodesInFile(Tree *T, FILE *dest)
+void saveNodesInFile(BinaryTree *T, FILE *dest)
 {
     struct s_arq_no temp;
     temp.chave = T->key;
@@ -295,7 +315,7 @@ void saveNodesInFile(Tree *T, FILE *dest)
     }
 }
 
-void saveSearchTreeInFile(Tree *T, const char *fileName)
+void saveBinaryTreeInFile(BinaryTree *T, const char *fileName)
 {
     FILE *treeFIle = openFile(fileName, "wb");
     if (treeFIle)
@@ -312,12 +332,12 @@ void saveSearchTreeInFile(Tree *T, const char *fileName)
     }
 }
 
-void createNodesFromFile(Tree **T, FILE *dest)
+void createNodesFromFile(BinaryTree **T, FILE *dest)
 {
     struct s_arq_no temp;
     if (fread(&temp, sizeof(struct s_arq_no), 1, dest))
     {
-        (*T) = newNodeSearchTree(temp.chave);
+        (*T) = newNodeBinaryTree(temp.chave);
         if (temp.esq)
         {
             createNodesFromFile(&(*T)->left, dest);
@@ -329,7 +349,7 @@ void createNodesFromFile(Tree **T, FILE *dest)
     }
 }
 
-void readSearchTreeFromFile(Tree **T, const char *fileName)
+void readBinaryTreeFromFile(BinaryTree **T, const char *fileName)
 {
     FILE *treeFile = openFile(fileName, "rb");
     if (treeFile)
@@ -360,14 +380,14 @@ void drawNode(int x, int y, int key)
     gfx_text(x + g_NODE_WIDTH / 2 - largura / 2, y + g_NODE_HEIGHT / 2 - altura / 2, v);
 }
 
-void drawTree(Tree *T, int x, int y, int dist)
+void drawTree(BinaryTree *T, int x, int y, int dist)
 {
     if (T)
     {
         dist /= 2;
         if (T->left)
         {
-            gfx_line(x, y + g_NODE_HEIGHT/2, x - dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT/2);
+            gfx_line(x, y + g_NODE_HEIGHT / 2, x - dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT / 2);
             drawTree(T->left, x - dist, y + g_NODES_DISTANCE, dist);
         }
         if (T->right)
@@ -381,11 +401,11 @@ void drawTree(Tree *T, int x, int y, int dist)
 
 int main()
 {
-    Tree *root = NULL, *aux;
+    BinaryTree *root = NULL, *aux;
     char opcAcao, fileName[20];
-    int inputkey;
+    int inputkey, S;
     gfx_init(g_SCREEN_WIDTH, g_SCREEN_HEIGHT, "Arvore de Busca");
-    do
+    while (1)
     {
         system("clear");
         printf("### Arvore Binaria de Busca ###\n[0] - Encerrar programa\
@@ -403,15 +423,15 @@ int main()
         {
         case '0':
             gfx_quit();
-            deleteSearchTree(&root);
+            deleteBinaryTree(&root);
             return 0;
             break;
         case '1':
-            if ((aux = maxSearchTree(root)))
+            if ((aux = maxBinaryTree(root)))
             {
                 printf("\nA maior chave na árvore é: %d", aux->key);
             }
-            if ((aux = minSearchTree(root)))
+            if ((aux = minBinaryTree(root)))
             {
                 printf("\nA menor chave na árvore é: %d\n", aux->key);
             }
@@ -424,55 +444,62 @@ int main()
             printf("\nInforme o valor da chave a ser buscada: ");
             scanf("%d", &inputkey);
             getchar();
-            if ((searchkeySearchTree(root, inputkey)))
+            searchkeyBinaryTree(root, inputkey, &S);
+            switch (S)
             {
-                printf("A chave está presente na árvore!\n");
-            }
-            else
-            {
-                printf("A chave não está presente na árvore!\n");
+            case 0:
+                printf("Arvore vazia!\n");
+                break;
+            case 1:
+                printf("A chave foi encontrada na arvore!\n");
+                break;
+            case 2:
+            case 3:
+                printf("O valor nao esta presente na arvore!\n");
+                break;
+            default:
+                break;
             }
             break;
         case '3':
             printf("\nInforme o valor da chave que deve ser encontrado o antecessor e sucessor: ");
             scanf("%d", &inputkey);
             getchar();
-            if (searchkeySearchTree(root, inputkey))
+            if ((aux = predecessorNodeBinaryTree(root, inputkey)))
             {
-                if ((aux = predecessorNodeSearchTree(root, inputkey)))
-                {
-                    printf("O antecessor da chave é: %d\n", aux->key);
-                }
-                else
-                {
-                    printf("A chave não possui antecessor!\n");
-                }
-                if ((aux = sucessorNodeSearchTree(root, inputkey)))
-                {
-                    printf("O sucessor da chave é: %d\n", aux->key);
-                }
-                else
-                {
-                    printf("A chave não possui sucessor!\n");
-                }
+                printf("O antecessor da chave é: %d\n", aux->key);
             }
             else
             {
-                printf("A chave não está presente na árvore!\n");
+                printf("A chave não possui antecessor!\n");
+            }
+            if ((aux = sucessorNodeBinaryTree(root, inputkey)))
+            {
+                printf("O sucessor da chave é: %d\n", aux->key);
+            }
+            else
+            {
+                printf("A chave não possui sucessor!\n");
             }
             break;
         case '4':
             printf("\nInforme o valor da chave que deve ser inserida: ");
             scanf("%d", &inputkey);
             getchar();
-            insertkeySearchTree(&root, inputkey);
-            printf("A chave %d foi inserida com sucesso!\n", inputkey);
+            if (insertKeyBinaryTree(&root, inputkey))
+            {
+                printf("A chave %d foi inserida com sucesso!\n", inputkey);
+            }
+            else
+            {
+                printf("Nao é possivel inserir valores repetidos na arvore de busca!\n");
+            }
             break;
         case '5':
             printf("\nInforme o valor da chave que deve ser removida: ");
             scanf("%d", &inputkey);
             getchar();
-            if (removekeySearchTree(&root, inputkey))
+            if (removeKeyBinaryTree(&root, inputkey))
             {
                 printf("A chave %d foi removida com sucesso!\n", inputkey);
             }
@@ -485,14 +512,14 @@ int main()
             printf("\nInforme o nome do arquivo em que a árvore está salva: ");
             scanf("%s", fileName);
             getchar();
-            deleteSearchTree(&root);
-            readSearchTreeFromFile(&root, fileName);
+            deleteBinaryTree(&root);
+            readBinaryTreeFromFile(&root, fileName);
             break;
         case '7':
             printf("\nInforme o nome do arquivo em que a árvore será salva: ");
             scanf("%s", fileName);
             getchar();
-            saveSearchTreeInFile(root, fileName);
+            saveBinaryTreeInFile(root, fileName);
             printf("Arquivo salvo com sucesso!\n");
             break;
         default:
@@ -504,6 +531,6 @@ int main()
         drawTree(root, g_X_TREE_ORIGIN, g_Y_TREE_ORIGIN, g_SCREEN_WIDTH / 2);
         gfx_paint();
         getchar();
-    } while (opcAcao != '0');
+    }
     return 0;
 }
