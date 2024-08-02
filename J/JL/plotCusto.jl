68162cdbfeb57f2faf8ad3fd54254db531979f5d
Erin <joaovitor14mju@gmail.com>
2024-08-02 13:04:27 -0400

    A

--- J/JL/plotCusto.jl
+++ J/JL/plotCusto.jl
@@ -1,7 +1,7 @@
 using Plots
 
 x = [i for i in 0:0.1:10]
-f(x) = -1 * x + 2
+f(x) = -100 * x + 3
 y = f.(x)
 
 cost(bias, weight) = (1 / (2 * length(x))) * sum((x * weight .+ bias - y).^2)
@@ -21,26 +21,30 @@ function partialDerivativeWeight(bias, weight)
     (1 / length(x)) * sum((x * weight .+ bias - y).*x)
 end
 
-bias = weight = range(-1000, 1000, length = 40)
+bias = range(-500, 500, length = 40)
+weight = range(-100, 100, length = 40)
 
 p_cost = plot3d(bias, weight, cost, xlabel = "Vies", ylabel = "Peso", st = :surface, title = "Minimização da função de custo", legend = false, marker = 2)
+scatter!([3], [-100], cost.([3], [-100]))
 
 bias = 0.0
 weight = 0.0
-alpha1 = 0.9
-alpha2 = 0.0000008
+alpha1 = 1
+alpha2 = 0.09
 
 @gif for i in 1:100
     global bias, weight, alpha1, alpha2
     scatter!([bias], [weight], cost.([bias], [weight]), legend = false)
     bias -= alpha1 * partialDerivativeBias(bias, weight)
     weight -= alpha2 * partialDerivativeWeight(bias, weight)
-end
+    println("(vies = $bias, peso = $weight")
+end every 1
 
 bias
 weight
+y
+x
 
-scatter(x, y)
 plot!(x, h(bias, weight), xlims = (1000, 1500))
 
 partialDerivativeBias(bias, weight)
