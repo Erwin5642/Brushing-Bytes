0d74b24db151f10f13499fdfdbb1aea10e15d575
Alice <joaovitor14mju@gmail.com>
2024-08-09 21:06:23 -0400

    Determinante perfeitado

--- J/C/Trabalhos/CN/det.c
+++ J/C/Trabalhos/CN/det.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 
-//Objetivo: multiplica uma linha de uma matriz por um escalar
+double mod(double v)
+{
+    return v >= 0 ? v : -v;
+}
+
+// Objetivo: multiplica uma linha de uma matriz por um escalar
 void escalarLinha(double escalar, double linha[10], int n)
 {
     while (n--)
@@ -10,7 +15,7 @@ void escalarLinha(double escalar, double linha[10], int n)
     }
 }
 
-//Objetivo: soma duas linhas e salva o resultado na linhaDest
+// Objetivo: soma duas linhas e salva o resultado na linhaDest
 void somaLinha(double linhaDest[10], double linhaSrc[10], int n)
 {
     while (n--)
@@ -19,7 +24,7 @@ void somaLinha(double linhaDest[10], double linhaSrc[10], int n)
     }
 }
 
-//Objetivo: troca os valores entre duas linhas
+// Objetivo: troca os valores entre duas linhas
 void trocaLinha(double linha1[10], double linha2[10], int n)
 {
     double linhaAux;
@@ -31,54 +36,70 @@ void trocaLinha(double linha1[10], double linha2[10], int n)
     }
 }
 
-//Objetivo: mostra os valores em uma matriz de forma formatada
-void mostrarMatriz(double matriz[10][10], unsigned short n){
+// Objetivo: mostra os valores em uma matriz de forma formatada
+void mostrarMatriz(double matriz[10][10], unsigned short n)
+{
     unsigned short i, j;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             printf("%.1lf ", matriz[i][j]);
         }
         printf("\n");
     }
 }
 
-//Objetivo: calcula o determinante de uma matriz
+// Objetivo: calcula o determinante de uma matriz
 double determinante(double matriz[10][10], unsigned short n)
 {
-    unsigned short i, j = 0, p = 1;
-    double det = 1, cm;
+    unsigned short i, j = 0, p = 1, indicePivo;
+    double det = 1, cm, pivo;
     printf("Passo 0:\n");
     mostrarMatriz(matriz, n);
     for (j = 0; j < n; j++)
     {
-        //Se o pivo for zero procura um candidato melhor
-        if (!matriz[j][j])
-        {
-            for(i = j + 1; i < n && !matriz[i][j]; i++){
-            }
-            //Encontrando o novo pivo, troca as linhas e atualiza a direção do determinante
-            if(i < n && matriz[i][j]){
-                trocaLinha(matriz[i], matriz[j], n);
-                det *= -1;
-            }
-        }
+        // Procura o maior numero em módulo
+        pivo = matriz[j][j];
+        indicePivo = j;
         for (i = j + 1; i < n; i++)
         {
-            printf("Passo %u:\n", p);
-            p++;
-            //Caso exista um pivo, simplifica a matriz
-            if (matriz[i][j])
+            if (mod(matriz[i][j]) > mod(pivo))
             {
-                cm = matriz[i][j] / matriz[j][j];
-                escalarLinha(-cm, matriz[j] + j, n - j);
-                somaLinha(matriz[i] + j, matriz[j] + j, n - j);
-                escalarLinha(-1 / cm, matriz[j] + j, n - j);
+                indicePivo = i;
             }
-            //Mostra o resultado do passo p
-            mostrarMatriz(matriz, n);
+        }
+        if (matriz[indicePivo][j])
+        {
+            //  Se o pivo não estiver na primeira linha, troca as linhas e atualiza a direção do determinante
+            if (indicePivo != j)
+            {
+                trocaLinha(matriz[indicePivo], matriz[j], n);
+                det *= -1;
+                printf("Passo %u:\n", p);
+                mostrarMatriz(matriz, n);
+                p++;
+            }
+
+            for (i = j + 1; i < n; i++)
+            {
+                if (matriz[i][j])
+                {
+                    printf("Passo %u:\n", p);
+                    p++;
+
+                    cm = matriz[i][j] / matriz[j][j];
+                    escalarLinha(-cm, matriz[j] + j, n - j);
+                    somaLinha(matriz[i] + j, matriz[j] + j, n - j);
+                    // Volta a linha com os valores anteriores
+                    escalarLinha(-1 / cm, matriz[j] + j, n - j);
+                    // Mostra o resultado do passo p
+                    mostrarMatriz(matriz, n);
+                }
+            }
         }
     }
-    //Calcula o determinante baseado na diagonal matriz triangular resultante
+    // Calcula o determinante baseado na diagonal matriz triangular resultante
     for (i = 0; i < n; i++)
     {
         det *= matriz[i][i];
@@ -92,18 +113,26 @@ int main()
     unsigned i, j, n;
     clock_t t;
     FILE *matrizes;
-    matrizes = fopen("testes.txt", "r");
-    while(fscanf(matrizes, "%u", &n) != EOF){
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n; j++){
+    // Abre o arquivo com as matrizes
+    matrizes = fopen("matrizes.txt", "r");
+    while (fscanf(matrizes, "%u", &n) != EOF)
+    { // Le o tamanho da matriz quadrada
+        // Le os valores da matriz
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
                 fscanf(matrizes, "%lf", &matriz[i][j]);
             }
         }
+        // Le o resultado esperado
         fscanf(matrizes, "%lf", &resultadoEsperado);
         t = clock();
         det = determinante(matriz, n);
+        // Calcula o tempo levado pela função
         t = clock() - t;
         tempo = ((double)t) / CLOCKS_PER_SEC;
+        // Exibe os resultados do calculo do determinante da matriz
         det == resultadoEsperado ? printf("---------VVVV--------\n") : printf("---------XXXX--------\n");
         printf("O resultado encontrado foi %lf e o resultado esperado era %lf\n\n", det, resultadoEsperado);
         printf("O algoritmo levou %lf segundos\n----------------------\n\n", tempo);
