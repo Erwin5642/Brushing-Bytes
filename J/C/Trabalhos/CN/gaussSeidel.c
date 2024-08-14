144e2a1f3d640b4fc3929e60a4ae343fc05ba43a
Alice <joaovitor14mju@gmail.com>
2024-08-13 21:28:04 -0400

    Seidel menu

--- J/C/Trabalhos/CN/gaussSeidel.c
+++ J/C/Trabalhos/CN/gaussSeidel.c
@@ -60,107 +60,135 @@ void trocaLinha(double linha1[10], double linha2[10], unsigned short n)
     }
 }
 
-int podeConverger(double linha[10], unsigned short n){
+int podeConvergir(double linha[10], unsigned short n)
+{
     unsigned short i, j;
     double somaLinha;
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++)
+    {
         somaLinha = 0;
-        for(j = 0; j < n; j++){
-            if(j != i){
-                somaLinha += absValue(linha[j]); 
+        for (j = 0; j < n; j++)
+        {
+            if (j != i)
+            {
+                somaLinha += absValue(linha[j]);
             }
         }
-        if(somaLinha < absValue(linha[i])){
+        if (somaLinha < absValue(linha[i]))
+        {
             return i;
         }
     }
     return -1;
 }
 
-int convergeSistema(SistemaLinear *Axb){
+int convergeSistema(SistemaLinear *Axb)
+{
     int i, n = Axb->nVariaveis, flag;
-    for(i = 0; i < n; i++){
-        if((flag = podeConverger(Axb->matrizExtendida[i], n)) != -1){
-            if(flag != i){
+    for (i = 0; i < n; i++)
+    {
+        if ((flag = podeConvergir(Axb->matrizExtendida[i], n)) != -1)
+        {
+            if (flag != i)
+            {
                 trocaLinha(Axb->matrizExtendida[i], Axb->matrizExtendida[flag], n + 1);
                 i = -1;
             }
         }
-        else{
+        else
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int gaussSeidel(SistemaLinear *Axb, double erro){
-    unsigned short i, j, n = Axb->nVariaveis, m = Axb->nConstantes; 
+int gaussSeidel(SistemaLinear *Axb, double erro)
+{
+    unsigned short i, j, n = Axb->nVariaveis, m = Axb->nConstantes, it = 1;
     double soma, erroAtual, solucaoAnterior[10];
-    if(!convergeSistema(Axb)){
+    if (!convergeSistema(Axb))
+    {
         return 0;
     }
-    do{
+    printf("Solução X(0): ");
     for(i = 0; i < n; i++){
-        soma = 0;
-        copiaVetor(solucaoAnterior, Axb->vetorSolucao, m);
-        for(j = 0; j < m; j++){
-            if(j != i){
-                soma += Axb->matrizExtendida[i][j] * Axb->vetorSolucao[j];
-            }
-        }
-        Axb->vetorSolucao[i] = (Axb->matrizExtendida[i][j] - soma) / Axb->matrizExtendida[i][i];
+        printf("%lf ", Axb->vetorSolucao[i]);
     }
-    erroAtual = calculaDKR(Axb->vetorSolucao, calculaDK(Axb->vetorSolucao, solucaoAnterior, m), m);
-    }while(erro < erroAtual);
+    printf("\n");
+    do
+    {
+        for (i = 0; i < n; i++)
+        {
+            soma = 0;
+            copiaVetor(solucaoAnterior, Axb->vetorSolucao, m);
+            for (j = 0; j < m; j++)
+            {
+                if (j != i)
+                {
+                    soma += Axb->matrizExtendida[i][j] * Axb->vetorSolucao[j];
+                }
+            }
+            Axb->vetorSolucao[i] = (Axb->matrizExtendida[i][j] - soma) / Axb->matrizExtendida[i][i];
+        }
+
+        printf("Solução X(%hu): ", it++);
+        for(i = 0; i < n; i++){
+            printf("%lf ", Axb->vetorSolucao[i]);
+        }
+        printf("\n");
+        erroAtual = calculaDKR(Axb->vetorSolucao, calculaDK(Axb->vetorSolucao, solucaoAnterior, m), m);
+    } while (erro < erroAtual);
     return 1;
 }
 
 int main()
 {
-    FILE *sistemasArq = fopen("sistemas.txt", "r");
     SistemaLinear Axb;
     clock_t t;
-    double tempo, erro, resultadoEsperado[10];
+    double tempo, erro;
     unsigned short i, j;
-    while (fscanf(sistemasArq, "%hu %hu", &Axb.nVariaveis, &Axb.nConstantes) != EOF)
+    do
     {
-        for(i = 0; i < Axb.nConstantes; i++){
-            for(j = 0; j < Axb.nVariaveis + 1; j++){
-                fscanf(sistemasArq, "%lf", &Axb.matrizExtendida[i][j]);
-            }
-        }
-        for(i = 0; i < Axb.nConstantes; i++){
-            fscanf(sistemasArq, "%lf", &resultadoEsperado[i]);
-        }
-        for(i = 0; i < Axb.nConstantes; i++){
-            fscanf(sistemasArq, "%lf", &Axb.vetorSolucao[i]);
-        }
-        fscanf(sistemasArq, "%lf", &erro);
-        t = clock();
-        if (gaussSeidel(&Axb, erro))
+        printf("Informe o numero de variaveis e de constantes:\n");
+        scanf("%hu %hu", &Axb.nVariaveis, &Axb.nConstantes);
+        if (Axb.nConstantes && Axb.nVariaveis)
         {
-
-            t = clock() - t;
-            tempo = ((double)t) / CLOCKS_PER_SEC;
-
-            printf("O resultado encontrado foi: ");
-            for (i = 0; i < Axb.nVariaveis; i++)
+            printf("Informe a matriz extendida do sistema:\n");
+            for (i = 0; i < Axb.nConstantes; i++)
             {
-                printf("%lf ", Axb.vetorSolucao[i]);
+                for (j = 0; j < Axb.nVariaveis + 1; j++)
+                {
+                    scanf("%lf", &Axb.matrizExtendida[i][j]);
+                }
             }
-            printf("\nO resultado esperado era: ");
-            for (i = 0; i < Axb.nVariaveis; i++)
+            printf("Informe a solução inicial:\n");
+            for (i = 0; i < Axb.nConstantes; i++)
             {
-                printf("%lf ", resultadoEsperado[i]);
+                scanf("%lf", &Axb.vetorSolucao[i]);
             }
-            printf("\n\nO algoritmo levou %lf segundos\n----------------------\n\n", tempo);
-        }
-        else
-        {
-            printf("O sistema não pode convergir!\n\n");
-        }
-    }
+            printf("Informe o erro:\n");
+            scanf("%lf", &erro);
+            t = clock();
+            if (gaussSeidel(&Axb, erro))
+            {
 
-    fclose(sistemasArq);
+                t = clock() - t;
+                tempo = ((double)t) / CLOCKS_PER_SEC;
+
+                printf("O resultado encontrado foi: ");
+                for (i = 0; i < Axb.nVariaveis; i++)
+                {
+                    printf("%lf ", Axb.vetorSolucao[i]);
+                }
+                printf("\n\nO algoritmo levou %lf segundos\n----------------------\n\n", tempo);
+            }
+            else
+            {
+                printf("O sistema não pode convergir!\n\n");
+            }
+        }
+    } while (Axb.nConstantes && Axb.nVariaveis);
+
     return 0;
 }
