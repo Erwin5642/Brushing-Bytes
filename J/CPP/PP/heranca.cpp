e912de6add465c10facd5f9372b23a31f757fb80
Johannes <135025081+Erwin5642@users.noreply.github.com>
2024-08-29 20:33:45 +0000

    PP herança

--- J/CPP/PP/heranca.cpp
+++ J/CPP/PP/heranca.cpp
@@ -8,14 +8,39 @@ public:
     Base(/* args */){
 
     }
+    Base(int i, int j){
+
+    }
     ~Base(){
         
+    }
+    int& at(unsigned){
 
     }
 };
 
-int main(){
 
+class Derivada : Base{
+    private:
+
+    public:
+        Derivada(){
+
+        }
+        Derivada(int i, int j){
+            Base(i, j);
+        }
+        ~Derivada(){
+
+        }
+        int at(unsigned){
+
+        }
+
+};
+
+int main(){
+    Base *pontBase;
 
     return 0;
-}
+} 
