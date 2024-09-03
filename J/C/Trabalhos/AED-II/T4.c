269dbffcfe828a340e9145599be016123f0347ef
Alice <joaovitor14mju@gmail.com>
2024-09-03 18:09:27 -0400

    Remoção

--- J/C/Trabalhos/AED-II/T4.c
+++ J/C/Trabalhos/AED-II/T4.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include "gfx.h"
+#include "gfx.h"
 #include <time.h>
 #include <unistd.h>
 
@@ -16,6 +16,36 @@ typedef struct AVLTree
     int key : 30, balance : 2;
     struct AVLTree *left, *right;
 } AVLTree;
+
+int rebalanceFromLeft(AVLTree **);
+int rebalanceFromRight(AVLTree **);
+
+typedef struct s_no
+{
+
+    int32_t chave : 28;
+
+    int32_t bal : 2;
+
+    int32_t reservado : 2; /* sem uso */
+
+    struct s_no *esq;
+
+    struct s_no *dir;
+
+} AVLNodeTree;
+
+typedef struct s_arq_no
+{
+    int32_t chave : 28;
+
+    int32_t bal : 2;
+
+    uint32_t esq : 1;
+
+    uint32_t dir : 1;
+
+} AVLNodeFile;
 
 void deleteAVLTree(AVLTree **Tree)
 {
@@ -70,22 +100,26 @@ void leftRightRotation(AVLTree **T, int f)
     {
         (*T)->left->balance = -1;
     }
-    if(f){
-         if(ptub == 0){
-             (*T)->left->balance = -1;
-             if(ptvb == 1){
-                
-             }
-             else{
-                 (*T)->balance = -1;
-             }
-         }
-    }  
+    if (f)
+    {
+        if (ptub == 0)
+        {
+            (*T)->balance = -1;
+            if (ptvb == 1)
+            {
+                rebalanceFromRight(&(*T)->left);
+            }
+            else
+            {
+                (*T)->left->balance = -1;
+            }
+        }
+    }
 }
 
 void rightLeftRotation(AVLTree **T, int f)
 {
-    int ptvb = (*T)->right->left->balance,  ptub = (*T)->right->balance;
+    int ptvb = (*T)->right->left->balance, ptub = (*T)->right->balance;
     rightRotation(&(*T)->right);
     leftRotation(T);
     if (ptvb == 1)
@@ -96,17 +130,83 @@ void rightLeftRotation(AVLTree **T, int f)
     {
         (*T)->right->balance = 1;
     }
-    if(f){
-         if(ptub == 0){
-             (*T)->right->balance = 1;
-             if(ptvb == -1){
-                
-             }
-             else{
-                 (*T)->balance = 1;
-             }
-         }
-    }  
+    if (f)
+    {
+        if (ptub == 0)
+        {
+            (*T)->balance = 1;
+            if (ptvb == -1)
+            {
+                rebalanceFromLeft(&(*T)->right);
+            }
+            else
+            {
+                (*T)->right->balance = 1;
+            }
+        }
+    }
+}
+
+int rebalanceFromRight(AVLTree **T)
+{
+    int flag = 1;
+    switch ((*T)->balance)
+    {
+    case 1:
+        (*T)->balance = 0;
+        break;
+    case 0:
+        (*T)->balance = -1;
+        flag = 0;
+        break;
+    case -1:
+        if (!(*T)->left->balance)
+        {
+            flag = 0;
+        }
+        if ((*T)->left->balance == -1)
+        {
+            rightRotation(T);
+        }
+        else
+        {
+            leftRightRotation(T, 1);
+        }
+        break;
+    }
+
+    return flag;
+}
+
+int rebalanceFromLeft(AVLTree **T)
+{
+    int flag = 1;
+
+    switch ((*T)->balance)
+    {
+    case -1:
+        (*T)->balance = 0;
+        break;
+    case 0:
+        (*T)->balance = 1;
+        flag = 0;
+        break;
+    case 1:
+        if (!(*T)->right->balance)
+        {
+            flag = 0;
+        }
+        if ((*T)->right->balance == 1)
+        {
+            leftRotation(T);
+        }
+        else
+        {
+            rightLeftRotation(T, 1);
+        }
+        break;
+    }
+    return flag;
 }
 
 // Retorna um ponteiro para o menor nó na árvore binária de busca
@@ -198,40 +298,6 @@ void insertKeyAVLTree(AVLTree **T, int key)
     }
 }
 
-// Remove um nó qualquer da arvore e retorna o nó que substituiu ele
-AVLTree *removeNodeAVLTree(AVLTree *node)
-{
-    AVLTree *aux, **sucessorPointer;
-    // Se houver filho a esquerda ou direita, apenas substitui o nó removido por um deles
-    if (!node->left)
-    {
-        aux = node->right;
-        free(node);
-        return aux;
-    }
-    if (!node->right)
-    {
-        aux = node->left;
-        free(node);
-        return aux;
-    }
-    // Se não, encontra o sucessor do nó a ser removido
-    sucessorPointer = &node->right;
-    while ((*sucessorPointer)->left)
-    {
-        sucessorPointer = &(*sucessorPointer)->left;
-    }
-    // Faz as trocas necessárias com os ponteiros e desaloca o nó
-    (*sucessorPointer)->left = node->left;
-    aux = *sucessorPointer;
-    *sucessorPointer = (*sucessorPointer)->right;
-    aux->right = node->right;
-    aux->balance = node->balance;
-    free(node);
-
-    return aux;
-}
-
 // Remove um nó da árvore a partir de uma chave
 // Retorna 1 num sucesso e 0 se houver algum erro
 void removeKeyAVLTree(AVLTree **T, int key)
@@ -262,6 +328,9 @@ void removeKeyAVLTree(AVLTree **T, int key)
                 aux = minBinaryTree((*T)->right);
                 (*T)->key = aux->key;
                 removeKeyAVLTree(&(*T)->right, aux->key);
+                if(flag){
+                    flag = rebalanceFromRight(T);
+                }
             }
             return;
         }
@@ -271,26 +340,7 @@ void removeKeyAVLTree(AVLTree **T, int key)
             removeKeyAVLTree(&(*T)->left, key);
             if (flag)
             {
-                switch ((*T)->balance)
-                {
-                case -1:
-                    (*T)->balance = 0;
-                    break;
-                case 0:
-                    (*T)->balance = 1;
-                    flag = 0;
-                    break;
-                case 1:
-                    if ((*T)->right->balance == 1)
-                    {
-                        leftRotation(T);
-                    }
-                    else
-                    {
-                        rightLeftRotation(T, 1);
-                    }
-                    break;
-                }
+                flag = rebalanceFromLeft(T);
             }
         }
         else if (key > (*T)->key)
@@ -299,155 +349,124 @@ void removeKeyAVLTree(AVLTree **T, int key)
             removeKeyAVLTree(&(*T)->right, key);
             if (flag)
             {
-                switch ((*T)->balance)
-                {
-                case 1:
-                    (*T)->balance = 0;
-                    break;
-                case 0:
-                    (*T)->balance = -1;
-                    flag = 0;
-                    break;
-                case -1:
-                    if ((*T)->left->balance == -1)
-                    {
-                        rightRotation(T);
-                    }
-                    else
-                    {
-                        leftRightRotation(T, 1);
-                    }
-                    break;
-                }
+                flag = rebalanceFromRight(T);
             }
         }
     }
 }
 
-// void drawNode(int x, int y, int key, int balance)
-// {
-//     char v[10];
-//     int largura, altura;
-//     gfx_set_color(0, 0, 0);
-//     gfx_filled_rectangle(x + 1, y + 1, x + g_NODE_WIDTH - 1, y + g_NODE_HEIGHT - 1);
-//     gfx_set_color(255, 255, 255);
-//     gfx_rectangle(x, y, x + g_NODE_WIDTH, y + g_NODE_HEIGHT);
-//     // Guarda o valor na string v
-//     sprintf(v, "%d", key);
-//     gfx_get_text_size(v, &largura, &altura);
-//     gfx_text(x + g_NODE_WIDTH / 2 - largura / 2, y + g_NODE_HEIGHT / 2 - altura / 2, v);
+void drawNode(int x, int y, int key, int balance)
+{
+    char v[10];
+    int largura, altura;
+    gfx_set_color(0, 0, 0);
+    gfx_filled_rectangle(x + 1, y + 1, x + g_NODE_WIDTH - 1, y + g_NODE_HEIGHT - 1);
+    gfx_set_color(255, 255, 255);
+    gfx_rectangle(x, y, x + g_NODE_WIDTH, y + g_NODE_HEIGHT);
+    // Guarda o valor na string v
+    sprintf(v, "%d", key);
+    gfx_get_text_size(v, &largura, &altura);
+    gfx_text(x + g_NODE_WIDTH / 2 - largura / 2, y + g_NODE_HEIGHT / 2 - altura / 2, v);
 
-//     sprintf(v, "%d", balance);
-//     gfx_get_text_size(v, &largura, &altura);
-//     gfx_text(x + g_NODE_WIDTH / 2 - largura / 2, y - altura, v);
-// }
+    sprintf(v, "%d", balance);
+    gfx_get_text_size(v, &largura, &altura);
+    gfx_text(x + g_NODE_WIDTH / 2 - largura / 2, y - altura, v);
+}
 
-// void drawTree(AVLTree *T, int x, int y, int dist)
-// {
-//     if (T)
-//     {
-//         dist /= 2;
-//         if (T->left)
-//         {
-//             gfx_line(x, y + g_NODE_HEIGHT / 2, x - dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT / 2);
-//             drawTree(T->left, x - dist, y + g_NODES_DISTANCE, dist);
-//         }
-//         if (T->right)
-//         {
-//             gfx_line(x, y + g_NODE_HEIGHT / 2, x + dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT / 2);
-//             drawTree(T->right, x + dist, y + g_NODES_DISTANCE, dist);
-//         }
-//         drawNode(x - g_NODE_WIDTH / 2, y, T->key, T->balance);
-//     }
-// }
+void drawTree(AVLTree *T, int x, int y, int dist)
+{
+    if (T)
+    {
+        dist /= 2;
+        if (T->left)
+        {
+            gfx_line(x, y + g_NODE_HEIGHT / 2, x - dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT / 2);
+            drawTree(T->left, x - dist, y + g_NODES_DISTANCE, dist);
+        }
+        if (T->right)
+        {
+            gfx_line(x, y + g_NODE_HEIGHT / 2, x + dist, y + g_NODES_DISTANCE + g_NODE_HEIGHT / 2);
+            drawTree(T->right, x + dist, y + g_NODES_DISTANCE, dist);
+        }
+        drawNode(x - g_NODE_WIDTH / 2, y, T->key, T->balance);
+    }
+}
 
-// // Operações em Arquivo
-// struct s_arq_no
-// {
-//     int32_t chave : 30;
-//     uint32_t esq : 1;
-//     uint32_t dir : 1;
-// };
+// Operações em Arquivo
+FILE *openFile(const char *name, const char *mode)
+{
+    FILE *file;
+    if ((file = fopen(name, mode)) == NULL)
+    {
+        printf("Não foi possível abrir o arquivo %s\n", name);
+    }
+    return file;
+}
 
-// FILE *openFile(const char *name, const char *mode)
-// {
-//     FILE *file;
-//     if ((file = fopen(name, mode)) == NULL)
-//     {
-//         printf("Não foi possível abrir o arquivo %s\n", name);
-//     }
-//     return file;
-// }
+void saveNodesInFile(AVLTree *T, FILE *dest)
+{
+    struct s_arq_no temp;
+    temp.chave = T->key;
+    temp.esq = T->left != NULL;
+    temp.dir = T->right != NULL;
+    fwrite(&temp, sizeof(struct s_arq_no), 1, dest);
+    if (T->left)
+    {
+        saveNodesInFile(T->left, dest);
+    }
+    if (T->right)
+    {
+        saveNodesInFile(T->right, dest);
+    }
+}
 
-// void saveNodesInFile(AVLTree *T, FILE *dest)
-// {
-//     struct s_arq_no temp;
-//     temp.chave = T->key;
-//     temp.esq = T->left != NULL;
-//     temp.dir = T->right != NULL;
-//     fwrite(&temp, sizeof(struct s_arq_no), 1, dest);
-//     if (T->left)
-//     {
-//         saveNodesInFile(T->left, dest);
-//     }
-//     if (T->right)
-//     {
-//         saveNodesInFile(T->right, dest);
-//     }
-// }
-
-// void saveSearchTreeInFile(AVLTree *T, const char *fileName)
-// {
-//     FILE *treeFIle = openFile(fileName, "wb");
-//     if (treeFIle)
-//     {
-//         if (T)
-//         {
-//             saveNodesInFile(T, treeFIle);
-//         }
-//         else
-//         {
-//             fputc(0, treeFIle);
-//         }
-//         fclose(treeFIle);
-//     }
-// }
+void saveSearchTreeInFile(AVLTree *T, const char *fileName)
+{
+    FILE *treeFIle = openFile(fileName, "wb");
+    if (treeFIle)
+    {
+        if (T)
+        {
+            saveNodesInFile(T, treeFIle);
+        }
+        else
+        {
+            fputc(0, treeFIle);
+        }
+        fclose(treeFIle);
+    }
+}
 
 int main()
 {
     AVLTree *T = NULL;
     srand(time(NULL));
 
-    // gfx_init(g_SCREEN_WIDTH, g_SCREEN_HEIGHT, "Arvore de Busca");
+    gfx_init(g_SCREEN_WIDTH, g_SCREEN_HEIGHT, "Arvore de Busca");
 
     int i, e;
-    for (i = 0; i < 20; i++)
+    for (i = 1; i < 50; i++)
     {
-        insertKeyAVLTree(&T, (rand() % 2) == 0 ? i + rand() % (1 + i) : -i + rand() % (i + 1));
-        // gfx_clear();
-        // drawTree(T, g_X_TREE_ORIGIN, g_Y_TREE_ORIGIN, g_SCREEN_WIDTH / 2);
-        // gfx_paint();
+        insertKeyAVLTree(&T, i);
+        gfx_clear();
+        drawTree(T, g_X_TREE_ORIGIN, g_Y_TREE_ORIGIN, g_SCREEN_WIDTH / 2);
+        gfx_paint();
     }
 
-    for (i = 0; i < 10; i++)
+    do
     {
-
-
         scanf("%d", &e);
         if (e)
         {
             removeKeyAVLTree(&T, e);
-            // gfx_clear();
-            // drawTree(T, g_X_TREE_ORIGIN, g_Y_TREE_ORIGIN, g_SCREEN_WIDTH / 2);
-            // gfx_paint();
-            // getchar();
+            gfx_clear();
+            drawTree(T, g_X_TREE_ORIGIN, g_Y_TREE_ORIGIN, g_SCREEN_WIDTH / 2);
+            gfx_paint();
+            getchar();
         }
-        else{
-            break;
-        }
-    }
+    } while (e);
 
     deleteAVLTree(&T);
-    // gfx_quit();
+    gfx_quit();
     return 0;
 }
