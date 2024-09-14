19ea6acd42bfa1d2ebe07cb2e7ae286e3c74bad9
Alice <joaovitor14mju@gmail.com>
2024-09-14 13:21:40 -0400

    LFA

--- J/Trabalhos/LFA/test.py
+++ J/Trabalhos/LFA/test.py
@@ -1,6 +1,111 @@
 import copy
 
-
+class NFA:
+    def __init__(self, no_state, states, no_alphabet, alphabets, start,
+                 no_final, finals, no_transition, transitions):
+        self.no_state = no_state
+        self.states = states
+        self.no_alphabet = no_alphabet
+        self.alphabets = alphabets
+         
+        # Adding epsilon alphabet to the list
+        # and incrementing the alphabet count
+        self.alphabets.append('e')
+        self.no_alphabet += 1
+        self.start = start
+        self.no_final = no_final
+        self.finals = finals
+        self.no_transition = no_transition
+        self.transitions = transitions
+ 
+        # Dictionaries to get index of states or alphabets
+        self.states_dict = dict()
+        for i in range(self.no_state):
+            self.states_dict[self.states[i]] = i
+        self.alphabets_dict = dict()
+        for i in range(self.no_alphabet):
+            self.alphabets_dict[self.alphabets[i]] = i
+             
+        # transition table is of the form
+        # [From State + Alphabet pair] -> [Set of To States]
+        self.transition_table = dict()
+        for i in range(self.no_state):
+            for j in range(self.no_alphabet):
+                self.transition_table[str(i)+str(j)] = []
+        for i in range(self.no_transition):
+            self.transition_table[str(self.states_dict[self.transitions[i][0]])
+                                  + str(self.alphabets_dict[
+                                      self.transitions[i][1]])].append(
+                                          self.states_dict[self.transitions[i][2]])
+ 
+    # Method to get input from User
+    @classmethod
+    def fromUser(cls):
+        states = list(input("States : ").split())
+        alphabets = list(input("Alphabets : ").split())
+        start = input("Start State : ")
+        finals = list(input("Final States : ").split())
+        transitions = list()
+        no_transition = int(input("Insira o numero de transições: "))
+        print("Enter Transitions (from alphabet to) (e for epsilon): ")
+        no_state = len(states)
+        no_alphabet = len(alphabets)
+        no_final = len(finals)
+        for i in range(no_transition):
+            transitions.append(input("-> ").split())
+        return cls(no_state, states, no_alphabet, alphabets, start,
+                   no_final, finals, no_transition, transitions)
+ 
+    # Method to represent quintuple
+    def __repr__(self):
+        return "Q : " + str(self.states)+"\nΣ : "
+        + str(self.alphabets)+"\nq0 : "
+        + str(self.start)+"\nF : "+str(self.finals) + \
+            "\nδ : \n" + str(self.transition_table)
+ 
+    def getEpsilonClosure(self, state):
+       
+        # Method to get Epsilon Closure of a state of NFA
+        # Make a dictionary to track if the state has been visited before
+        # And a array that will act as a stack to get the state to visit next
+        closure = dict()
+        closure[self.states_dict[state]] = 0
+        closure_stack = [self.states_dict[state]]
+ 
+        # While stack is not empty the loop will run
+        while (len(closure_stack) > 0):
+           
+            # Get the top of stack that will be evaluated now
+            cur = closure_stack.pop(0)
+             
+            # For the epsilon transition of that state,
+            # if not present in closure array then add to dict and push to stack
+            for x in self.transition_table[
+                    str(cur)+str(self.alphabets_dict['e'])]:
+                if x not in closure.keys():
+                    closure[x] = 0
+                    closure_stack.append(x)
+            closure[cur] = 1
+        return closure.keys()
+ 
+    def getStateName(self, state_list):
+       
+        # Get name from set of states to display in the final DFA diagram
+        name = ''
+        for x in state_list:
+            name += self.states[x]
+        return name
+ 
+    def isFinalDFA(self, state_list):
+       
+        # Method to check if the set of state is final state in DFA
+        # by checking if any of the set is a final state in NFA
+        for x in state_list:
+            for y in self.finals:
+                if (x == self.states_dict[y]):
+                    return True
+        return False
+ 
 class DFA:
     def __init__(self, states, alphabets, init_state, final_states, transition_funct):
         self.states = states
@@ -12,10 +117,9 @@ class DFA:
         self.ds = {}
         self.transition_dict = {}
         self.set_transition_dict()
-        # self.draw_graph('', 'first')
 
     def set_transition_dict(self):
-        dict_states = {r: {c: 'ϕ' for c in self.states} for r in self.states}
+        dict_states = {r: {c: 'e' for c in self.states} for r in self.states}
         for i in self.states:
             for j in self.states:
                 indices = [ii for ii, v in enumerate(self.transition_funct[i]) if v == j]  # get indices of states
@@ -28,13 +132,13 @@ class DFA:
         return [state for state in self.states if state not in ([self.init_state] + self.final_states)]
 
     def get_predecessors(self, state):
-        return [key for key, value in self.ds.items() if state in value.keys() and value[state] != 'ϕ' and key != state]
+        return [key for key, value in self.ds.items() if state in value.keys() and value[state] != 'e' and key != state]
 
     def get_successors(self, state):
-        return [key for key, value in self.ds[state].items() if value != 'ϕ' and key != state]
+        return [key for key, value in self.ds[state].items() if value != 'e' and key != state]
 
     def get_if_loop(self, state):
-        if self.ds[state][state] != 'ϕ':
+        if self.ds[state][state] != 'e':
             return self.ds[state][state]
         else:
             return ''
@@ -50,8 +154,8 @@ class DFA:
             # print('predecessor : ', predecessors)
             # print('successor : ', successors)
 
-            # dd = {r: {c: 'ϕ' for c in self.states if c != inter} for r in self.states if r != inter}
-            ##dd = {r: {c: 'ϕ' for c in temp_states if c != inter} for r in temp_states if r != inter}
+            # dd = {r: {c: 'e' for c in self.states if c != inter} for r in self.states if r != inter}
+            ##dd = {r: {c: 'e' for c in temp_states if c != inter} for r in temp_states if r != inter}
             for i in predecessors:
                 for j in successors:
                     inter_loop = self.get_if_loop(inter)
@@ -72,8 +176,61 @@ class DFA:
         #print('Regular Expression : ', re)
         return re
 
-
 def main():
+    nfa = NFA.fromUser()
+
+    # Finding epsilon closure beforehand so to not recalculate each time
+    epsilon_closure = dict()
+    for x in nfa.states:
+        epsilon_closure[x] = list(nfa.getEpsilonClosure(x))
+    
+    # First state of DFA will be epsilon closure of start state of NFA
+    # This list will act as stack to maintain till when to evaluate the states
+    dfa_stack = list()
+    dfa_stack.append(epsilon_closure[nfa.start])
+    
+    # List to store the states of DFA
+    dfa_states = list()
+    dfa_states.append(epsilon_closure[nfa.start])
+    
+    # Loop will run till this stack is not empty
+    while (len(dfa_stack) > 0):
+        # Getting top of the stack for current evaluation
+        cur_state = dfa_stack.pop(0)
+    
+        # Traversing through all the alphabets for evaluating transitions in DFA
+        for al in range((nfa.no_alphabet) - 1):
+            # Set to see if the epsilon closure of the set is empty or not
+            from_closure = set()
+            for x in cur_state:
+                # Performing Union update and adding all the new states in set
+                from_closure.update(
+                    set(nfa.transition_table[str(x)+str(al)]))
+    
+            # Check if epsilon closure of the new set is not empty
+            if (len(from_closure) > 0):
+                # Set for the To state set in DFA
+                to_state = set()
+                for x in list(from_closure):
+                    to_state.update(set(epsilon_closure[nfa.states[x]]))
+    
+                # Check if the to state already exists in DFA and if not then add it
+                if list(to_state) not in dfa_states:
+                    dfa_stack.append(list(to_state))
+                    dfa_states.append(list(to_state))
+    
+            # Else case for empty epsilon closure
+            # This is a dead state(ϕ) in DFA
+            else:
+            
+                # Check if any dead state was present before this
+                # if not then make a new dead state ϕ
+                if (-1) not in dfa_states:
+    
+                    # Adding -1 to list to mark that dead state is present
+                    dfa_states.append(-1)
+ 
+ 
     states = input('Enter the states in your DFA : ')
     states = states.split()
     alphabets = input('Enter the alphabets : ')
@@ -97,6 +254,12 @@ def main():
     # print(dfa.transition_dict)
     # print(dfa.ds)
 
+    print(states)
+    print(alphabets)
+    print(init_state)
+    print(final_states)
+    print(transition_matrix)
+    print(transition_funct)
 
 if __name__ == '__main__':
     main()
