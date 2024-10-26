3f7a0d0ebd488a382b807bcce882d273bc1be817
sofia <joaovitor14mju@gmail.com>
2024-10-26 19:58:32 -0400

    Heap

--- J/Trabalhos/AED-II/T6/T6_Compacta.c
+++ J/Trabalhos/AED-II/T6/T6_Compacta.c
@@ -5,34 +5,38 @@ typedef struct ListNode
 {
     struct ListNode *next;
     char character;
-}List;
+} List;
 
-typedef struct HuffmanNode{
+typedef struct HuffmanNode
+{
     struct HuffmanNode *children[2];
     unsigned frequency;
     char character;
-}HuffmanTrie;
+} HuffmanTrie;
 
-typedef struct{
+typedef struct
+{
     unsigned *nodes;
     List *characters;
-    size_t n, m;
-}Table;
+    int n, m;
+} Table;
 
-typedef struct  
+typedef struct
 {
     HuffmanTrie **array;
-    size_t n, m;
-}Heap;
+    int n, m;
+} Heap;
 
-List *newListNode(char character){
+List *newListNode(char character)
+{
     List *aux = malloc(sizeof(List));
     aux->next = NULL;
     aux->character = character;
     return aux;
 }
 
-void insertList(List **L, char newChar){
+void insertList(List **L, char newChar)
+{
     List *aux;
     aux = *L;
     *L = newListNode(newChar);
@@ -40,32 +44,32 @@ void insertList(List **L, char newChar){
 }
 
 // Desaloca os nós da Lista Encadeada Simples
-void deleteList(List **L)
+List *deleteList(List *L)
 {
     List *aux;
-    // Garante que o ponteiro apontava para algo
-    if (L != NULL)
+    // Enquanto não chegar no fim da lista, salva o próximo nó à *L em aux, desaloca *L
+    // e reconecta *L com o restante da lista em aux
+    while (L != NULL)
     {
-        // Enquanto não chegar no fim da lista, salva o próximo nó à *L em aux, desaloca *L
-        // e reconecta *L com o restante da lista em aux 
-        while (*L != NULL)
-        {
-            aux = (*L)->next;
-            free(*L);
-            *L = aux;
-        }
+        aux = (L)->next;
+        free(L);
+        L = aux;
     }
+    return NULL;
 }
 
-Table createFrequencyTable(char *text, size_t size){
+Table createFrequencyTable(char *text, int size)
+{
     Table newTable;
-    size_t i;
+    int i;
     newTable.nodes = calloc(256, sizeof(unsigned));
     newTable.m = 256;
     newTable.n = 0;
     newTable.characters = NULL;
-    for(i = 0; i < size; i++){
-        if(newTable.nodes[text[i]] == 0){
+    for (i = 0; i < size; i++)
+    {
+        if (newTable.nodes[(unsigned)text[i]] == 0)
+        {
             newTable.n++;
             insertList(&newTable.characters, text[i]);
         }
@@ -74,95 +78,156 @@ Table createFrequencyTable(char *text, size_t size){
     return newTable;
 }
 
-// void swapNode(HuffmanTrie **a, HuffmanTrie **b){
-//     HuffmanTrie **aux = a;
-//     *a = *b;
-//     *b = *aux;
-// }
+void deleteTable(Table *table)
+{
+    free(table->nodes);
+    table->nodes = NULL;
+    table->characters = deleteList(table->characters);
+    table->m = 0;
+    table->n = 0;
+}
 
-// HuffmanTrie *newTrieNode(char newChar, unsigned newFreq){
-//     HuffmanTrie *aux = malloc(sizeof(HuffmanTrie));
-//     aux->character = newChar;
-//     aux->frequency = newFreq;
-//     return aux;
-// }
+void swapNode(HuffmanTrie **a, HuffmanTrie **b)
+{
+    HuffmanTrie *aux;
+    aux = *b;
+    *b = *a;
+    *a = aux;
+}
 
-// void descendHeap(Heap heap, size_t i){
-//     size_t j = 2 * i + 1, n = heap.n;
-//     if(j < n){
-//         if(j < n - 1){
-//             if(heap.array[j + 1]->frequency < heap.array[j]->frequency){
-//                 j++;
-//             }
-//         }
-//         if(heap.array[i]->frequency < heap.array[j]->frequency){
-//             swapNode(&heap.array[i], &heap.array[j]);
-//             descendHeap(heap, i);
-//         }
-//     }
-// }
+HuffmanTrie *newTrieNode(char newChar, unsigned newFreq)
+{
+    HuffmanTrie *aux = malloc(sizeof(HuffmanTrie));
+    aux->character = newChar;
+    aux->frequency = newFreq;
+    aux->children[0] = aux->children[1] = NULL;
+    return aux;
+}
 
-// void ascendHeap(Heap heap, unsigned index){
-//     int j = (index - 1)/2;
-//     if(j >= 0){
-//         if(heap.array[j]->frequency > heap.array[index]->frequency){
-//             swapNode(&heap.array[j], &heap.array[index]);
-//             ascendHeap(heap, j);
-//         }
-//     }
-// }
+void descendHeap(Heap heap, int i)
+{
+    int j = 2 * i + 1, n = heap.n;
+    if (j < n)
+    {
+        if (j < n - 1)
+        {
+            if (heap.array[j + 1]->frequency < heap.array[j]->frequency)
+            {
+                j++;
+            }
+        }
+        if (heap.array[i]->frequency < heap.array[j]->frequency)
+        {
+            swapNode(&heap.array[i], &heap.array[j]);
+            descendHeap(heap, i);
+        }
+    }
+}
 
-// void insertHeap(Heap* heap, HuffmanTrie *newNode){
-//     if(heap->n < heap->m){
-//         heap->array[heap->n] = newNode;
-//         ascendHeap(*heap, heap->n);
-//         heap->n++;
-//     }
-// }
+void ascendHeap(Heap heap, int index)
+{
+    int j = (index - 1) / 2;
+    if (j >= 0 && index)
+    {
+        if (heap.array[j]->frequency > heap.array[index]->frequency)
+        {
+            swapNode(&(heap.array[j]), &(heap.array[index]));
+            ascendHeap(heap, j);
+        }
+    }
+}
 
-// HuffmanTrie *removeHeap(Heap *ptheap){
-//     HuffmanTrie *aux;
-//     if(ptheap->n){
-//         aux = ptheap->array[0];
-//         ptheap->array[0] = ptheap->array[ptheap->n - 1];
-//         ptheap->n--;
-//         descendHeap(*ptheap, 0);
-//         return aux;
-//     }
-//     return NULL;
-// }
+void insertHeap(Heap *heap, HuffmanTrie *newNode)
+{
+    if (heap->n < heap->m)
+    {
+        heap->array[heap->n] = newNode;
+        ascendHeap(*heap, heap->n);
+        heap->n++;
+    }
+}
 
-// Heap createMinHeap(Table t){
-//     Heap newHeap;
-//     size_t i;
-//     List *auxList = t.characters;
-//     newHeap.array = malloc(t.n * sizeof(HuffmanTrie *));
-//     newHeap.m = t.n;
-//     newHeap.n = 0;
-//     for(i = 0; i < t.n; i++){
-//         insertHeap(&newHeap, newTrieNode(t.characters->character, t.nodes[(unsigned)t.characters->character]));
-//         auxList = auxList->next;
-//     }
-//     return newHeap;
-// }
+HuffmanTrie *removeHeap(Heap *ptheap)
+{
+    HuffmanTrie *aux;
+    if (ptheap->n)
+    {
+        aux = ptheap->array[0];
+        ptheap->array[0] = ptheap->array[ptheap->n - 1];
+        ptheap->n--;
+        descendHeap(*ptheap, 0);
+        return aux;
+    }
+    return NULL;
+}
 
-// HuffmanTrie *mergeTries(HuffmanTrie *t1, HuffmanTrie *t2){
-//     HuffmanTrie *aux = newTrieNode(' ', t1->frequency + t2->frequency);
-//     aux->children[0] = t1;
-//     aux->children[1] = t2;
-//     return aux;    
-// }
+Heap createMinHeap(Table table)
+{
+    Heap newHeap;
+    List *auxList = table.characters;
+    newHeap.array = malloc(table.n * sizeof(HuffmanTrie *));
+    newHeap.m = table.n;
+    newHeap.n = 0;
+    while (auxList)
+    {
+        insertHeap(&newHeap, newTrieNode(auxList->character, table.nodes[(unsigned)auxList->character]));
+        auxList = auxList->next;
+    }
+    return newHeap;
+}
 
-// HuffmanTrie *createHuffmanTrie(Heap *heap){
-//     HuffmanTrie *aux1, *aux2;
-//     while (heap->n > 1)
-//     {
-//         aux1 = removeHeap(heap);
-//         aux2 = removeHeap(heap);
-//         insertHeap(heap, mergeTries(aux1, aux2));
-//     }
-//     return heap->array[0];
-// }
+void deleteHeap(Heap *ptheap)
+{
+    free(ptheap->array);
+    ptheap->array = NULL;
+    ptheap->n = 0;
+    ptheap->m = 0;
+}
+
+HuffmanTrie *mergeTries(HuffmanTrie *t1, HuffmanTrie *t2)
+{
+    HuffmanTrie *aux = newTrieNode('\0', t1->frequency + t2->frequency);
+    aux->children[0] = t1;
+    aux->children[1] = t2;
+    return aux;
+}
+
+HuffmanTrie *createHuffmanTrie(Heap *heap)
+{
+    HuffmanTrie *aux1, *aux2;
+    while (heap->n > 1)
+    {
+        aux1 = removeHeap(heap);
+        aux2 = removeHeap(heap);
+        insertHeap(heap, mergeTries(aux1, aux2));
+    }
+    return heap->array[0];
+}
+
+void showHuffmanTrie(HuffmanTrie *trie){
+    if (trie->children[0])
+    {
+        printf("Freq: %d\n", trie->frequency);
+        showHuffmanTrie(trie->children[0]);
+        showHuffmanTrie(trie->children[1]);
+    }
+    else{
+        printf("Freq: %d Char: %c\n", trie->frequency, trie->character);
+    }
+}
+
+HuffmanTrie *deleteHuffmanTrie(HuffmanTrie *trie)
+{
+    if(trie){
+        if (trie->children[0])
+        {
+            deleteHuffmanTrie(trie->children[0]);
+            deleteHuffmanTrie(trie->children[1]);
+        }
+        free(trie);
+    }
+    return NULL;
+}
 
 // Operações em Arquivo
 // Abre um arquivo
@@ -178,10 +243,10 @@ FILE *openFile(const char *name, const char *mode)
 
 // Aloca um vetor e armazena o conteúdo de um arquivo binário nele e retorna um ponteiro para os dados
 // e o tamanho do conjunto
-char *readTextFromFile(const char *fileName, size_t *size)
+char *readTextFromFile(const char *fileName, int *size)
 {
     FILE *file = openFile(fileName, "rb");
-    size_t bytes;
+    int bytes;
     char *t;
     if (file)
     {
@@ -194,7 +259,7 @@ char *readTextFromFile(const char *fileName, size_t *size)
         // Salva todos os bytes no vetor
         fread(t, bytes, 1, file);
         fclose(file);
-        // Divide o número de bytes por 4 (32 bits) para obter o tamanho do conjunto
+        // Divide o número de bytes pelo tamanho do char para obter o tamanho do conjunto
         *size = bytes / sizeof(char);
         return t;
     }
@@ -203,7 +268,7 @@ char *readTextFromFile(const char *fileName, size_t *size)
 }
 
 // Salva o conteúdo de um vetor em um arquivo binário
-int saveTextInFile(char *text, size_t size, const char *fileName)
+int saveTextInFile(char *text, int size, const char *fileName)
 {
     FILE *file = openFile(fileName, "wb");
     if (file)
@@ -215,23 +280,30 @@ int saveTextInFile(char *text, size_t size, const char *fileName)
     return 0;
 }
 
-void compressFile(char *filename){
-    size_t size;
+void compressFile(char *filename)
+{
+    int size;
     char *text = readTextFromFile(filename, &size);
-    Table frequencias;
-    if(text){
-        frequencias = createFrequencyTable(text, size);
-
-        free(frequencias.nodes);
+    Table frequency;
+    Heap priorityListNodes;
+    HuffmanTrie *codesTrie;
+    if (text)
+    {
+        frequency = createFrequencyTable(text, size);
+        priorityListNodes = createMinHeap(frequency);
+        codesTrie = createHuffmanTrie(&priorityListNodes);
+        showHuffmanTrie(codesTrie);
+        deleteTable(&frequency);
+        deleteHeap(&priorityListNodes);
+        codesTrie = deleteHuffmanTrie(codesTrie);
         free(text);
     }
 }
 
-
-void decompressFile(char *filename){
+void decompressFile(char *filename)
+{
     filename[0] = 1;
 }
-
 
 int main(int argc, char *argv[])
 {
